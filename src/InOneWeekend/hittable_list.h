b0b3a8b5135fbdd63a9453a124e5388ef54a76c8
say-no-to-sleep <genezhao63@proton.me>
2026-07-12 20:53:36 -0400

    6.5 Done

---
+++ src/InOneWeekend/hittable_list.h
@@ -0,0 +1,65 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+// allow the code to write shared_ptr<hittable> rather than std::shared_ptr<hittable>.
+// as of 6.5, we're not using make_shared yet... 
+using std::make_shared;
+using std::shared_ptr;
+
+// inherits from hittable
+// Allows code to work with a single abstract object World even if it's a collection of objects
+class hittable_list : public hittable {
+    public:
+        // pointers to hittable objects
+        std::vector<shared_ptr<hittable>> objects;
+
+        // default constructor is empty
+        hittable_list() {}
+        // create a list of 1 object, actual insertion in `add`
+        hittable_list(shared_ptr<hittable> object) { add(object); }
+
+        // destroys the vector’s copies of the shared pointers
+        void clear() { objects.clear(); }
+
+        // copies the shared pointer
+        void add(shared_ptr<hittable> object) {
+            // append it to the list of hittable objects
+            objects.push_back(object);
+        }
+        
+        // same hit operation for every shape
+        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+            // temporary storage
+            hit_record temp_rec;
+            // was any intersection found
+            bool hit_anything = false;
+            // closest_so_far is the maximum distance currently worth considering
+            // at the beginning, no intersection has been found, so any hit up to the caller’s original ray_tmax is acceptable
+            // when an object was hit, it updates to the closer one and then so on and so forth
+            auto closest_so_far = ray_tmax;
+
+            // examine every object.
+            for (const auto& object : objects) {
+                // ask the OBJECT if the ray hits it within interval from tmin to closest so far (which updates)
+                if (object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
+                    // it was hit
+                    // we hit SOMETHING
+                    hit_anything = true;
+                    // shrinks the ray distance to this object
+                    // future objects must be closer than this to be accounted for
+                    closest_so_far = temp_rec.t;
+                    // Save the closest hit record found so far.
+                    rec = temp_rec;
+                }
+            }
+            // true if at least 1 item was hit
+            return hit_anything;
+        }
+};
+
+#endif
