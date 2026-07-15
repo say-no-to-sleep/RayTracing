62609a56a6c6a029bafc4f844604db18257af895
say-no-to-sleep <genezhao63@proton.me>
2026-07-15 14:55:55 -0400

    9.1 Done

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -18,7 +18,7 @@ int main() {
     camera cam;
     
     cam.aspect_ratio = 16.0/9.0;
-    cam.image_width = 200;
+    cam.image_width = 1920;
     cam.samples_per_pixel = 100;
 
     cam.render(world);
--- src/InOneWeekend/vec3.h
+++ src/InOneWeekend/vec3.h
@@ -52,6 +52,15 @@ class vec3 {
         double length_squared() const {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
+
+        static vec3 random() {
+            return vec3(random_double(), random_double(), random_double());
+        }
+
+        static vec3 random(double min, double max) {
+            return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+        }
+
 };
 
 // point3 is alias for vec3, useful for geometric clarity in code.
@@ -108,6 +117,26 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
+}
+
+// random direction needed for diffuse
+inline vec3 random_unit_vector() {
+    while (true) {
+        // get a random point inside the cube
+        auto p = vec3::random(-1,1);
+        // calculate how far the point is from the center. 
+        auto lensq = p.length_squared();
+        // Is the point inside the sphere and not essentially zero
+        if (1e-160 < lensq && lensq <= 1) return p / sqrt(lensq); // normalization
+    }
+}
+
+// set random vector for diffuse to correct hemisphere
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    // if dot is positive, mean they are on the same side
+    if(dot(on_unit_sphere, normal) > 0.0) return on_unit_sphere;
+    else return -on_unit_sphere;
 }
 
 #endif
